c9188e76b60e1d526cd17f2a3f4b213cc3a82ad7
RadiantK <pps8853@gmail.com>
2021-12-12 00:25:37 +0900

    Vue 공부

---
+++ src/components/Fruit.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default { // 객체 리터럴 반환
+  props: {
+    name: {
+      type : String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  h1 {
+    color: red !important;
+  }
+</style>
