c42521748417f97fab62e9cee28dfdfb8b1eae2f
RadiantK <pps8853@gmail.com>
2021-12-12 15:56:11 +0900

    class, style 바인딩

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,29 @@
   <h1>{{ reversedMessage }}</h1> -->
 
   <!-- part4. watch -->
-  <h1 @click="changeMessage">
+  <!-- <h1 @click="changeMessage">
     {{ msg }}
   </h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1> -->
+
+  <!-- part 5. class바인딩, style바인딩 -->
+  <!-- active에 클래스속성을 추가하고싶은데 true값일때 추가가 되므로
+  문자를 클릭해서 isActive가 true값이 될 때 active 클래스가 추가 -->
+  <!-- 클래스속성에 연결하는것이 객체데이터이므로 key value 형태로
+   여러가지의 클래스를 연결 가능  -->
+  <!-- <h1
+    :class="{active: isActive}"
+    @click="activate">
+    Hello?!({{ isActive }})
+  </h1> -->
+
+  <!-- part5.5 style바인딩  -->
+  <!-- 여러개의 객체데이터를 이용할 때 배열구문 사용 -->
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
+  </h1>
 </template>
 
 <script> 
@@ -103,45 +122,91 @@
 // }
 
 // part4. watch
+// export default {
+//   data() {
+//     return {
+//       msg: 'Hello?'
+//     }
+//   },
+//   computed: {
+//     reversedMessage() {
+//       return this.msg.split('').reverse().join('')
+//     }
+//   },
+//   // 특정한 데이터들의 변경사항을 감시하는 용도
+//   // 하나의 메소드처럼 만들어서 데이터의 로직으로 
+//   // 그 데이터가 변경됐을 때 어떤 내용이 실행될지 명시를 해주면 됨
+//   watch: {
+//     msg(newValue) { // 매개변수이름을 원하는 이름을 넣어줘도 됨
+//       console.log('msg:', newValue)
+//     },
+//     reversedMessage() {
+//       console.log('reversedMessage: ', this.reversedMessage)
+//     }
+//   },
+//   methods: {
+//     changeMessage() {
+//       this.msg= 'Good!'
+//     }
+//   }
+// }
+
+
+// part 5. class바인딩
+// export default {
+//   data() {
+//     return {
+//       isActive: false
+//     }
+//   },
+//   methods: {
+//     activate(){
+//       this.isActive = true
+//     }
+//   }
+// }
+
+
+// part5.5 style바인딩 
 export default {
   data() {
     return {
-      msg: 'Hello?'
-    }
-  },
-  computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
-    }
-  },
-  // 특정한 데이터들의 변경사항을 감시하는 용도
-  // 하나의 메소드처럼 만들어서 데이터의 로직으로 
-  // 그 데이터가 변경됐을 때 어떤 내용이 실행될지 명시를 해주면 됨
-  watch: {
-    msg(newValue) { // 매개변수이름을 원하는 이름을 넣어줘도 됨
-      console.log('msg:', newValue)
-    },
-    reversedMessage() {
-      console.log('reversedMessage: ', this.reversedMessage)
+      // color: 'orange',
+      // fontSize: '30px'
+      fontStyle: { // 위랑 같은의미
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
     }
   },
   methods: {
-    changeMessage() {
-      this.msg= 'Good!'
+    changeStyle() {
+      this.fontStyle.color = 'blue'
+      this.fontStyle.fontSize = '50px'
     }
   }
 }
+
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
   // part 1
-  // h1 {
-  //   font-size: 50px;
-  //   color: royalblue;
-  // }
-  // ul {
-  //   li {
-  //     font-size: 40px;
-  //   }
-  // }
+  /* h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+  ul {
+    li {
+      font-size: 40px;
+    }
+  } */
+
+  //
+ .active {
+    color: red;
+    font-weight: bold;
+  }
 </style>
